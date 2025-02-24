80571e113409e23f907f29ce151a88d7119a8acc
nouman <samdhekamdhe17@gmail.com>
2025-02-24 21:12:21 +0500

    working on blog crud

--- src/app/controllers/blog-controller.js
+++ src/app/controllers/blog-controller.js
@@ -1,10 +1,20 @@
-import { createBlog } from "../services";
+import { createBlog, editBlog } from "../services";
 
 class BlogController {
   async create(req, res) {
-    const { status, json } = await createBlog(req.body);
+    const { status, json } = await createBlog(req.body, req.files);
+    return res.status(status).json(json);
+  }
+
+  async edit(req, res) {
+    const { status, json } = await editBlog(req.body, req.params, req.files);
+    return res.status(status).json(json);
+  }
+
+  async getSingle(req, res) {
+    const { status, json } = await editBlog(req.params);
     return res.status(status).json(json);
   }
 }
 
-export const { create } = new BlogController();
+export const { create, edit } = new BlogController();
--- src/app/services/blog-services.js
+++ src/app/services/blog-services.js
@@ -2,21 +2,52 @@ import { invalidResponse, successfulResponse } from "@/utils";
 import { Blogs } from "../models";
 
 export class BlogServices {
-  async createBlog(body) {
+  async createBlog(body, files) {
     const { title, description, user } = body;
     if (!title || !description || !user) {
       return invalidResponse('All fields are required!');
     }
-    let image;
-    if (body.image) {
-      image = body.image;
+    let media;
+    if (files) {
+      media = files.media[0].filename;
     }
-    const blog = await Blogs.create({ title, description, image, user });
+    const blog = await Blogs.create({ title, description, media, user });
     if (!blog) {
       return invalidResponse('Blog creation failed!');
     }
 
     return successfulResponse('Blog created successfully!', blog);
   }
+
+  async editBlog(body, params, files) {
+    const { id } = params;
+    if (!id) {
+      return invalidResponse('Blog id is required!');
+    }
+
+    const { title, description, user } = body;
+    let media;
+    if (files) {
+      media = files.media[0].filename;
+    }
+    const blog = await Blogs.findByIdAndUpdate(
+      id,
+      { title, description, media, user },
+      { new: true }
+    )
+    if (!blog) {
+      return invalidResponse('Blog update failed!');
+    }
+
+    return successfulResponse('Blog updated successfully!', blog);
+  }
+
+  async getSingle(id) {
+    const blog = await Blogs.findById(id);
+    if (!blog) {
+      return invalidResponse('Blog not found!');
+    }
+    return successfulResponse('Blog found successfully!', blog);
+  }
 }
-export const { createBlog } = new BlogServices();
+export const { createBlog, editBlog, getSingle } = new BlogServices();
